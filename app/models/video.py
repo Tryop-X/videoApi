fdf312c51f7b1e18abd91da9b9ed26ed262a34c0
Hugo Vera <hugo10vs@hotmail.com>
2023-11-24 11:02:09 -0500

    prueba con youtube-api

--- app/models/video.py
+++ app/models/video.py
@@ -1,29 +1,132 @@
-class Contenido:
-    def __init__(self, start="", end="", content=""):
-        self.start = start
-        self.end = end
-        self.content = content
+from typing import List
 
-    def to_dict(self):
-        return {
-            'start': self.start,
-            'end': self.end,
-            'content': self.content
-        }
-class Video:
-    def __init__(self, url="", transcription=None, description=None, resume="", category=""):
+class Thumbnail:
+    def __init__(self, url: str, width: int, height: int):
         self.url = url
-        self.transcription = transcription
-        self.description = description
-        self.resume = resume
-        self.category = category
-        self.cod = category
+        self.width = width
+        self.height = height
 
     def to_dict(self):
         return {
             'url': self.url,
-            'transcription': [t.to_dict() for t in self.transcription] if self.transcription else None,
-            'description': self.description,
-            'resume': self.resume,
-            'category': self.category
+            'width': self.width,
+            'height': self.height
         }
+
+class Thumbnails:
+    def __init__(self, default: Thumbnail, medium: Thumbnail, high: Thumbnail):
+        self.default = default
+        self.medium = medium
+        self.high = high
+
+    def to_dict(self):
+        return {
+            'default': self.default.to_dict(),
+            'medium': self.medium.to_dict(),
+            'high': self.high.to_dict()
+        }
+
+class Snippet:
+    def __init__(self, publishedAt: str, channelId: str, title: str, description: str, thumbnails: Thumbnails, channelTitle: str, liveBroadcastContent: str, publishTime: str):
+        self.publishedAt = publishedAt
+        self.channelId = channelId
+        self.title = title
+        self.description = description
+        self.thumbnails = thumbnails
+        self.channelTitle = channelTitle
+        self.liveBroadcastContent = liveBroadcastContent
+        self.publishTime = publishTime
+
+    def to_dict(self):
+        return {
+            'publishedAt': self.publishedAt,
+            'channelId': self.channelId,
+            'title': self.title,
+            'description': self.description,
+            'thumbnails': self.thumbnails.to_dict(),
+            'channelTitle': self.channelTitle,
+            'liveBroadcastContent': self.liveBroadcastContent,
+            'publishTime': self.publishTime
+        }
+
+class Id:
+    def __init__(self, kind: str, videoId: str):
+        self.kind = kind
+        self.videoId = videoId
+
+    def to_dict(self):
+        return {
+            'kind': self.kind,
+            'videoId': self.videoId
+        }
+
+class Item:
+    def __init__(self, kind: str, etag: str, id: Id, snippet: Snippet):
+        self.kind = kind
+        self.etag = etag
+        self.id = id
+        self.snippet = snippet
+
+    def to_dict(self):
+        return {
+            'kind': self.kind,
+            'etag': self.etag,
+            'id': self.id.to_dict(),
+            'snippet': self.snippet.to_dict()
+        }
+
+class PageInfo:
+    def __init__(self, totalResults: int, resultsPerPage: int):
+        self.totalResults = totalResults
+        self.resultsPerPage = resultsPerPage
+
+    def to_dict(self):
+        return {
+            'totalResults': self.totalResults,
+            'resultsPerPage': self.resultsPerPage
+        }
+
+class YoutubeObject:
+    def __init__(self, kind: str, etag: str, nextPageToken: str, regionCode: str, pageInfo: PageInfo, items: List[Item]):
+        self.kind = kind
+        self.etag = etag
+        self.nextPageToken = nextPageToken
+        self.regionCode = regionCode
+        self.pageInfo = pageInfo
+        self.items = items
+
+    def to_dict(self):
+        return {
+            'kind': self.kind,
+            'etag': self.etag,
+            'nextPageToken': self.nextPageToken,
+            'regionCode': self.regionCode,
+            'pageInfo': self.pageInfo.to_dict(),
+            'items': [item.to_dict() for item in self.items] if self.items else None
+        }
+
+class Aspecto:
+    def __init__(self, aspecto: str, temas: List[str]):
+        self.aspecto = aspecto
+        self.temas = temas
+
+    def to_dict(self):
+        return {
+            'aspecto': self.aspecto,
+            'temas': self.temas
+        }
+
+class TemarioObject:
+    def __init__(self, temaCentral: str, aspectos: List[Aspecto], items: List[Item]):
+        self.temaCentral = temaCentral
+        self.aspectos = aspectos
+        self.items = items
+
+    def to_dict(self):
+        return {
+            'temaCentral': self.temaCentral,
+            'aspectos': [aspecto.to_dict() for aspecto in self.aspectos] if self.aspectos else None,
+            'items': [item.to_dict() for item in self.items] if self.items else None
+        }
+
+
--- app/services/videoRepository.py
+++ app/services/videoRepository.py
@@ -1,13 +1,18 @@
+from typing import List
+
 from pymongo import MongoClient
-from app.Emb.EmbeddingService import EmbeddingService
-import numpy as np
-from scipy.spatial.distance import cosine
-
-embeddingService = EmbeddingService()
-
+from googleapiclient.discovery import build
+from app.models import video as models
 
 class DataBaseConnection:
     def __init__(self, database_name="my_database", collection_name="my_collection"):
+
+        self.API_KEY = "AIzaSyC_SSAcmJZgl_tBU4QuCnUpLCLikBOXkGI"
+        self.YOUTUBE_API_SERVICE_NAME = 'youtube'
+        self.YOUTUBE_API_VERSION = 'v3'
+
+        #################################################################################################################
+
         self.MONGO_URI = "mongodb+srv://hveras1:GlDC38DAX913ZSIv@videoai.mkgw6wh.mongodb.net/?retryWrites=true&w=majority"
         # self.MONGO_URI = "mongodb://127.0.0.1:27017/"
         self.DATABASE_NAME = database_name
@@ -20,162 +25,43 @@ class DataBaseConnection:
         except Exception as e:
             print(f"Error connecting to database: {e}")
 
-    def search_video_embedding(self, query):
-        try:
-            emb = embeddingService.get_embedding(query)
-            return self.search_videos_by_embedding(emb)
-        except Exception as e:
-            print(f"Error in search_video: {e}")
-
-    def search_video(self, palabra):
-        try:
-            pipeline = self.consulta(palabra)
-            result = list(self.collection.aggregate(pipeline))
-            return result
-        except Exception as e:
-            print(f"Error inserting videos: {e}")
+    # def search_video_embedding(self, query):
+    #     try:
+    #         emb = embeddingService.get_embedding(query)
+    #         return self.search_videos_by_embedding(emb)
+    #     except Exception as e:
+    #         print(f"Error in search_video: {e}")
+    #
+    # def search_video(self, palabra):
+    #     try:
+    #         pipeline = self.consulta(palabra)
+    #         result = list(self.collection.aggregate(pipeline))
+    #         return result
+    #     except Exception as e:
+    #         print(f"Error inserting videos: {e}")
 
     def close(self):
         if self.client:
             self.client.close()
 
-    # def search_videos_by_embeddiang(self, query_embedding):
-    #     try:
-    #
-    #                 # Calcular la similitud coseno entre los dos embeddings aplanados
-    #                 similarity_score = 1 - cosine(embedding1_flat, embedding2_flat)
-    #
-    #                 sim = cosine_similarity(query_embedding, transcript_embedding)[0][0]
-    #                 if sim > similarity_threshold:
-    #                     similarities.append((video, sim))
-    #
-    #
-    #             all_embeddings = [np.array(transcript['embedding']) for transcript in video['transcription']]
-    #
-    #
-    #
-    #             if all_embeddings:
-    #                 # Calcular el promedio de los embeddings
-    #                 avg_embedding = np.mean(all_embeddings, axis=0)
-    #                 avg_embedding = avg_embedding.reshape(1, -1)  # Reformar a dos dimensiones
-    #
-    #                 sim = cosine_similarity([query_embedding], [avg_embedding])[0][0]
-    #                 if sim > similarity_threshold:
-    #                     similarities.append((video, sim))
-    #
-    #         # Ordenar por similitud
-    #         sorted_videos = sorted(similarities, key=lambda x: x[1], reverse=True)
-    #         return [video for video, sim in sorted_videos]
-    #
-    #     except Exception as e:
-    #         print(f"Error searching videos: {e}")
-    #         return []
+    def buscar_videos(self, palabra_clave, max_resultados=2) -> List[models.Item]:
+        youtube = build(self.YOUTUBE_API_SERVICE_NAME, self.YOUTUBE_API_VERSION, developerKey=self.API_KEY)
 
-    def search_videos_by_embedding(self, data):
+        # Hacer la solicitud para buscar videos por palabra clave
+        request = youtube.search().list(q=palabra_clave, part='id,snippet', maxResults=max_resultados)
+        response = request.execute()
 
-        videos = list(self.collection.find({}, {"cod": 1, "title": 1, "url": 1, "transcription": 1}))
-        similarity_threshold = 0.5  # Umbral de similitud
+        return response.items
 
-        for temas in data:
+    def get_temario_videos(self, temario: models.TemarioObject) -> models.TemarioObject:
 
-            pregunta1 = temas["preguntas"][0]["pregunta"]
-            pregunta2 = temas["preguntas"][1]["pregunta"]
-            pregunta3 = temas["preguntas"][2]["pregunta"]
+        for asp in temario.aspectos:
 
-            for video in videos:
+            lista_items = list()
+            for tema in asp.temas:
+                items = self.buscar_videos(temario.temaCentral + " " + asp.aspecto + " " + tema)
+                lista_items.append(items)
 
-                embedding1_flat = np.ravel(embeddingService.get_embedding(pregunta1))
-                embedding2_flat = np.ravel(embeddingService.get_embedding(pregunta2))
-                embedding3_flat = np.ravel(embeddingService.get_embedding(pregunta3))
+            asp.items = lista_items
 
-                s1 = 0
-                s2 = 0
-                s3 = 0
-
-                for transcript in video['transcription']:
-
-                    if transcript.get('embedding') is not None:
-
-                        transcript_embedding = np.ravel(transcript['embedding'])
-                        similarity_score1 = 1 - cosine(transcript_embedding, embedding1_flat)
-                        similarity_score2 = 1 - cosine(transcript_embedding, embedding2_flat)
-                        similarity_score3 = 1 - cosine(transcript_embedding, embedding3_flat)
-
-                        if (s1 < similarity_score1):
-                            s1 = similarity_score1
-                        if (s2 < similarity_score2):
-                            s1 = similarity_score1
-                        if (s3 < similarity_score3):
-                            s1 = similarity_score1
-
-                video["s1"] = s1
-                video["s2"] = s2
-                video["s3"] = s3
-
-            vidtema1 = max(videos, key=lambda x: x["s1"])
-            vidtema2 = max(videos, key=lambda x: x["s2"])
-            vidtema3 = max(videos, key=lambda x: x["s3"])
-
-            temas["videos"] = [
-                {
-                    'url': vidtema1["url"],
-                    'cod': vidtema1["cod"],
-                    'title': vidtema1["title"],
-                    'similarity': vidtema1["s1"]
-                },
-                {
-                    'url': vidtema2["url"],
-                    'cod': vidtema2["cod"],
-                    'title': vidtema2["title"],
-                    'similarity': vidtema2["s1"]
-                },
-                {
-                    'url': vidtema3["url"],
-                    'cod': vidtema3["cod"],
-                    'title': vidtema3["title"],
-                    'similarity': vidtema3["s1"]
-                },
-            ]
-
-        return data
-
-    def consulta(self, palabra):
-        pipeline = [
-            {
-                "$match": {
-                    "transcription": {
-                        "$elemMatch": {
-                            "content": {
-                                "$regex": palabra,
-                                "$options": 'i'
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "url": 1,
-                    "transcription": {
-                        "$filter": {
-                            "input": "$transcription",
-                            "as": "transcript",
-                            "cond": {
-                                "$regexMatch": {
-                                    "input": "$$transcript.content",
-                                    "regex": palabra,
-                                    "options": "i"
-                                }
-                            }
-                        }
-                    },
-                    "description": 1,
-                    "resume": 1,
-                    "cod": 1,
-                    "category": 1,
-                    "title": 1
-                }
-            }
-        ]
-        return pipeline
+        return temario
